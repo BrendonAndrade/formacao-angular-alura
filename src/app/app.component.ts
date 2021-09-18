c11ccf14efc01e57331295aee7d4a65d7d4dfcf1
Brendon Andrade <brendon@ciandt.com>
2021-09-18 12:17:27 -0300

    Aula 3 - Utilizando Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+  transferir($event){
+    console.log($event);
+    this.transferencia = $event;
+  }
 }
